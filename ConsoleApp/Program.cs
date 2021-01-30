94109850d2e4b79764365833b9ec7cd95530e439
analaura100 <19470201@itcampeche.edu.mx>
2021-01-29 19:55:11 -0600

    Agrega las opciones Agregar Autor y Consultar autores.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -14,22 +14,22 @@ namespace ConsoleApp
             {
                 Console.WriteLine("[A]gregar autor | [M]odificar autor | [E]liminar autor | [V]erautores | [S]alir"); Console.Write("Selecciona una opción: "); lectura = Char.ToUpper(Console.ReadKey().KeyChar); Console.WriteLine(); switch (lectura)
                 {
-                    case 'A':AddAuthor(); break;
+                    case 'A': AddAuthor(); break;
                     case 'M': ModifyAuthor(); break;
                     case 'E': DeleteAuthor(); break;
                     case 'V': ShowAuthors("Autores registrados"); break;
                     case 'S': Console.WriteLine("Adiós. Programa finalizado."); break;
                     default: break;
                 }
-               Console.WriteLine();
+                Console.WriteLine();
             } while (lectura != 'S'); Console.WriteLine("");
-               
+
         }
 
         private static void ShowAuthors(string text)
         {
             var authors = context.Authors.ToList();
-            Console.WriteLine(value: $"{text}: Se han registrado {authors.Count} autores."); 
+            Console.WriteLine(value: $"{text}: Se han registrado {authors.Count} autores.");
             foreach (var author in authors)
             {
                 Console.WriteLine(author.FirstName + " " + author.LastName);
@@ -48,12 +48,12 @@ namespace ConsoleApp
 
         private static void AddAuthor()
         {
-            Console.WriteLine("Agregando un autor."); 
-            Console.Write("Nombres: "); 
-            string firstName = Console.ReadLine(); 
-            Console.Write("Apellidos: "); 
+            Console.WriteLine("Agregando un autor.");
+            Console.Write("Nombres: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Apellidos: ");
             string lastName = Console.ReadLine();
-            var author = new Author { FirstName = firstName, LastName = lastName }; 
+            var author = new Author { FirstName = firstName, LastName = lastName };
             context.Authors.Add(author); context.SaveChanges();
         }
     }
